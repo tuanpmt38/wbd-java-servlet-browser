32cc0af39a99c5d9d07be7a7fe12e8a4b16af501
minhtuan <tuanpmt38@gmail.com>
2018-04-27 16:18:03 +0700

    wbd java browser

--- src/BrowserServlet.java
+++ src/BrowserServlet.java
@@ -4,14 +4,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "BrowserServlet")
+@WebServlet(name = "BrowserServlet", urlPatterns = "/browser")
 public class BrowserServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userAgent = request.getHeader("User-Agent");
+        PrintWriter writer = response.getWriter();
+        String browser = "Unknow browser";
+        if(userAgent.contains("Firefox")){
+            browser = "Mozilla Firefox";
+        } else if(userAgent.contains("OPR")){
+            browser = "Opera";
+        } else if(userAgent.contains("Safari")) {
+            if(userAgent.contains("Chrome")){
+                browser = "Google Chrome";
+            } else {
+                browser = "Apple Safari";
+            }
+        }
 
+        writer.println("<html>");
+        writer.println("<h1>Browser Detector</h1>");
+        writer.println("<h3>Your User-Agent Is: " + userAgent + "</h3>");
+        writer.println("<h3>Your Browser Is: " + browser + "</h3>");
+        writer.println("</html>");
     }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -8,9 +8,9 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-    <title>$Title$</title>
+    <title>Browser Detector</title>
   </head>
   <body>
-  $END$
+  <a href="/browser">Show my browser</a>
   </body>
 </html>
